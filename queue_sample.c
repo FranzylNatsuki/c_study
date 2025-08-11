a6c2239447c10832b8c4d418da871a88e047b7de
Franz Macalua <franzylbjornlmacalua@su.edu.ph>
2025-08-11 22:29:33 +0800

    new file

---
+++ queue_sample.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct node *nd;
+struct node {
+	int data;
+	nd next;
+} QUEUE;
+
+void enqueue(nd *front, nd *rear, int data) {
+	if ((*front) == NULL) { 
+		(*front) = malloc(sizeof(QUEUE));
+		(*rear) = (*front);
+		(*rear)->data = data;
+	} else {	
+		nd temp = malloc(sizeof(QUEUE));
+		(*rear)->next = temp;
+		(*rear) = temp;
+		(*rear)->data = data;
+		(*rear)->next = NULL;
+	}
+}
+
+void dequeue(nd *front, nd *rear) {
+
+}
+
+int main(void) {
+
+}
